436eba0fadd83879072801620e38e32facd260e0
Erez <shmuelie@post.bgu.ac.il>
2020-04-07 10:53:53 +0300

    erez commit3

--- dev/Main.java
+++ dev/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[]args){
-
         System.out.println("Hello World");
         System.out.println("check for commit to branch");
         System.out.println("hide erez");
