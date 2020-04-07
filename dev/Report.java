6c9b854a0be52a52a9b14d893b06e36030b290ba
or kandabi <kandabior@gmail.com>
2020-04-07 14:28:16 +0300

    add classes

--- dev/Report.java
+++ dev/Report.java
@@ -2,17 +2,38 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Report {
+    private static  int globalId=1;
 
+    private int reportId;
     private String title;
     private List<ReportLine> lines;
 
     private Report(String title){
+        reportId=globalId++;
         this.title=title;
         lines=new LinkedList<>();
     }
 
-    private Report makeReport(){
-        return null;
+    public static Report makeCategoryReport(List<Product> products){
+        Report report= new Report("Category Report");
+        report.lines= makeReportLines(products);
+        return report;
+    }
+
+    public static Report makeDefectiveReport(List<Product> products){
+        Report report= new Report("Defective Product Report");
+        report.lines= makeReportLines(products);
+        return report;
+    }
+
+    public static Report makeMissingReport(List<Product> products){
+        Report report= new Report("Missing Product Report");
+        report.lines= makeReportLines(products);
+        return report;
+    }
+
+    public static List<ReportLine> makeReportLines(List<Product> products){
+        return ReportLine.makeReportLines(products);
     }
 
 
--- dev/ReportLine.java
+++ dev/ReportLine.java
@@ -1,2 +1,40 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class ReportLine {
+
+    private int prodId;
+    private String prodName;
+    private int storageAmount;
+    private int shelfAmount;
+    private List<String> categories;
+
+    private ReportLine(int prodId, String name, int StorageAmount,int shelffAmount, List<String> categories ){
+        this.prodId= prodId;
+        this.prodName=name;
+        this.storageAmount=StorageAmount;
+        this.shelfAmount=shelffAmount;
+        this.categories=categories;
+    }
+
+    public static List<ReportLine> makeReportLines(List<Product> products){
+        List<ReportLine> output= new LinkedList<>();
+        for (Product product:products) {
+
+
+        }
+        return output;
+    }
+
+
+    @Override
+    public String toString() {
+        return "{" +
+                "prodId=" + prodId +
+                ", prodName='" + prodName + '\'' +
+                ", storageAmount=" + storageAmount +
+                ", shelfAmount=" + shelfAmount +
+                ", categories=" + categories +
+                '}';
+    }
 }
