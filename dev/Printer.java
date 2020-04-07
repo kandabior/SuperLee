ff2274ba25aebdd34b5f1fcdd1ee26410fd30c79
or kandabi <kandabior@gmail.com>
2020-04-07 11:59:34 +0300

    add classes

--- dev/Printer.java
+++ dev/Printer.java
@@ -1,2 +1,17 @@
 public class Printer {
+    private static Printer instance;
+
+    private Printer(){
+    }
+
+    public static Printer getPrinter(){
+        if(instance==null){
+            instance=new Printer();
+        }
+        return instance;
+    }
+
+    public String printStock(){
+        return null;
+    }
 }
--- dev/Report.java
+++ dev/Report.java
@@ -1,2 +1,19 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Report {
+
+    private String title;
+    private List<ReportLine> lines;
+
+    private Report(String title){
+        this.title=title;
+        lines=new LinkedList<>();
+    }
+
+    private Report makeReport(){
+        return null;
+    }
+
+
 }
