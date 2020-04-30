8f34b631d60e97e4b126e41276d8c138dc9963e8
Elad Solomon <Eladsolomon@gmail.com>
2020-04-30 16:44:12 +0300

    add 1.0

---
+++ dev/InterfaceLayer/OrderController.java
@@ -0,0 +1,94 @@
+import LogicLayer.Order;
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class OrderController {
+
+    List<Order> orders;
+    private static OrderController order_sp = null;
+
+    private OrderController() {
+        this.orders = new LinkedList<>();
+    }
+
+    public static OrderController getOrderController() {
+        if (order_sp == null)
+            order_sp = new OrderController();
+        return order_sp;
+    }
+
+    public boolean addOrder(int id, List<Pair<Integer, Integer>> items, int supplierId) {
+        Order order = new Order(id, items, supplierId);
+        boolean result = order.makeOrder();
+        if (result) {
+            orders.add(order);
+            return true;
+        }
+        return false;
+    }
+
+    public int getOrdersSize() {
+        return this.orders.size();
+    }
+
+    public List<Pair<Integer, Integer>> getItemsInOrderById(int id) {
+        return this.orders.get(id).getItemsInOrderById();
+    }
+
+    public int getSupplierIdOfOrder(int i) {
+        return this.orders.get(i).getSupplierIdOfOrder();
+    }
+
+    public double getTotalOrderMoney(int orderId) {
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.get(i).getId() == orderId) {
+                return orders.get(i).getTotalOrderMoney();
+            }
+        }
+        return -1;
+
+    }
+
+    public void setOrderCost(int orderId, double totalMoney) {
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.get(i).getId() == orderId) {
+                orders.get(i).setTotalCost(totalMoney);
+            }
+
+        }
+    }
+    public int getOrderIdByIndex(int i) {
+        return this.orders.get(i).getId();
+    }
+
+    public boolean checkIfOrderExists(int orderId) {
+        for (int i =0 ;i<orders.size();i++)
+        {
+            if (orders.get(i).getId()==orderId)
+                return true;
+        }
+        return false;
+    }
+
+    public void updateOrderStatus(int orderId) {
+        for (int i =0;i<orders.size();i++)
+        {
+            if(orders.get(i).getId()==orderId)
+            {
+                orders.get(i).setStatus();
+                break;
+            }
+        }
+    }
+
+    public String getOrderStatus(int orderId) {
+        for (int i = 0; i < orders.size(); i++) {
+            if (orders.get(i).getId() == orderId) {
+                return orders.get(i).getStatus();
+            }
+        }
+        return "";
+    }
+}
---
+++ dev/LogicLayer/Agreement.java
@@ -0,0 +1,70 @@
+package LogicLayer;
+
+import javafx.util.Pair;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Agreement {
+
+    private LinkedHashMap<Integer, Double> terms;
+    private BillOfQuantities bill;
+
+    public Agreement() { //i love you dorin
+        this.terms = new LinkedHashMap<>();
+        this.bill = null;
+    }
+
+    public void addBillOfQuantities(Map<Integer, Pair<Integer, Double>> bill) {
+        this.bill = new BillOfQuantities(bill);
+    }
+
+    public void updateBillOfQuantities( Integer itemId, Pair<Integer, Double> quantity_disc) {
+        this.bill.updateBillOfQuantities(itemId,quantity_disc);
+    }
+
+    public void deleteFromBillOfQuantities(Integer itemId) {
+        this.bill.deleteFromBillOfQuantities(itemId);
+    }
+
+    public void deleteBillOfQuantities() { this.bill = null; }
+
+    public void insertItem(Integer itemId, Double price) {
+        terms.put(itemId, price);
+    }
+
+    public LinkedHashMap<Integer, Double> getTerms() { return this.terms; }
+
+    public void setPrice(int id, double newPrice) {
+        terms.replace(id, newPrice);
+    }
+
+    public double getPriceOfItem(int index) {
+        return terms.get(index);
+    }
+
+    public Boolean checkBillOfQuantity() {
+        return this.bill != null;
+    }
+
+    public void addItemToBillOfQuantities(int itemId, int itemQuantity, Double itemDiscount) {
+        this.bill.addItemToBillOfQuantities(itemId,itemQuantity,itemDiscount);
+    }
+
+    public Map<Integer, Pair<Integer, Double>> getBillOfQuantities() {
+        return this.bill.getBillOfQuantities();
+    }
+
+    public int getBillSize() { return this.bill.getBillSize(); }
+
+    public double getOrderCost(int itemId, int quantity) {
+        if (bill != null && bill.checkItemInBill(itemId, quantity) == true) {
+            double discount = bill.getMoneyAmountofItemInBill(itemId, quantity);
+            double cost = terms.get(itemId);
+            double costMulQuantity = (cost * quantity);
+            return costMulQuantity * (1 - discount);
+        } else {
+            double cost = terms.get(itemId);
+            return (cost * quantity);
+        }
+    }
+}
