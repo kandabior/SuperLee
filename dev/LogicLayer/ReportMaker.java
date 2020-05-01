2260ad532dbe017fcf69b668ccf3d526024f2757
or kandabi <kandabior@gmail.com>
2020-05-01 17:49:45 +0300

    update

--- dev/LogicLayer/ReportMaker.java
+++ dev/LogicLayer/ReportMaker.java
@@ -23,54 +23,54 @@ public class ReportMaker {
         return instance;
     }
 
-    public String printStock(List<Pair<Integer,Integer>> quantity){
-        Report report=Report.totalStockReport(quantity);
+    public String printStock(int branchId,List<Pair<Integer,Integer>> quantity){
+        Report report=Report.totalStockReport(branchId,quantity);
         reports.add(report);
         return report.toString();
     }
 
-    public String printMissingProducts(List<Pair<Integer,Integer>> quantity){
-        Report report=Report.makeMissingReport(quantity);
+    public String printMissingProducts(int branchId,List<Pair<Integer,Integer>> quantity){
+        Report report=Report.makeMissingReport(branchId,quantity);
         reports.add(report);
         return report.toString();
     }
 
-    public String MakeDefectiveReport(List<Pair<Integer,Integer>> quantity){
-        Report report=Report.makeDefectiveReport(quantity);
-        reports.add(report);
-        return report.toString();
-    }
-
-
-
-    public String printByCategories(List<Pair<Integer,Integer>> quantity){
-        Report report=Report.makeCategoryReport(quantity);
-        reports.add(report);
-        return report.toString();
-
-    }
-
-
-    public String printSaleProductPrices(Pair<Integer, List<Double>> prices) {
-        Report report= Report.makeSalePriceReport(prices);
-        reports.add(report);
-        return report.toString();
-    }
-    public String printCostProductPrices(Pair<Integer, List<Double>> prices) {
-        Report report= Report.makeCostPriceReport(prices);
+    public String MakeDefectiveReport(int branchId,List<Pair<Integer,Integer>> quantity){
+        Report report=Report.makeDefectiveReport(branchId,quantity);
         reports.add(report);
         return report.toString();
     }
 
 
-    public String printShelfReport(List<Pair<Integer, Integer>> shelfStock) {
-        Report report= Report.makeShelfReport(shelfStock);
+
+    public String printByCategories(int branchId,List<Pair<Integer,Integer>> quantity){
+        Report report=Report.makeCategoryReport(branchId,quantity);
+        reports.add(report);
+        return report.toString();
+
+    }
+
+
+    public String printSaleProductPrices(int branchId,Pair<Integer, List<Double>> prices) {
+        Report report= Report.makeSalePriceReport(branchId,prices);
+        reports.add(report);
+        return report.toString();
+    }
+    public String printCostProductPrices(int branchId,Pair<Integer, List<Double>> prices) {
+        Report report= Report.makeCostPriceReport(branchId,prices);
         reports.add(report);
         return report.toString();
     }
 
-    public String printStorageReport(List<Pair<Integer, Integer>> storageStock) {
-        Report report= Report.makeStorageReport(storageStock);
+
+    public String printShelfReport(int branchId,List<Pair<Integer, Integer>> shelfStock) {
+        Report report= Report.makeShelfReport(branchId,shelfStock);
+        reports.add(report);
+        return report.toString();
+    }
+
+    public String printStorageReport(int branchId,List<Pair<Integer, Integer>> storageStock) {
+        Report report= Report.makeStorageReport(branchId,storageStock);
         reports.add(report);
         return report.toString();
     }
