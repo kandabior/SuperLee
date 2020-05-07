c80de0810557661370e77a789c74aed3aaf0828f
dorinma <dorinma@post.bgu.ac.il>
2020-05-07 20:44:28 +0300

    methods in supplier mapper

--- dev/DataAccessLayer/SupplierMapper.java
+++ dev/DataAccessLayer/SupplierMapper.java
@@ -1,6 +1,8 @@
 package DataAccessLayer;
 
 import java.sql.*;
+import javafx.util.Pair;
+import java.util.*;
 
 public class SupplierMapper {
 
@@ -47,17 +49,73 @@ public class SupplierMapper {
         return false;
     }
 
-/*
- public void addBillOfQuantities(int supplierId,Map<Integer, Pair<Integer, Double>> bill) {
-        if (getSuppById(supplierId) != null) {
-            getSuppById(supplierId).getAgreement().addBillOfQuantities(bill);
+
+ public boolean addBillOfQuantities(int supplierId, Map<Integer, Pair<Integer, Double>> bill) {
+     try {
+         Class.forName("org.sqlite.JDBC");
+         conn = DriverManager.getConnection("jdbc:sqlite:EOEDdatabase.db");
+         conn.setAutoCommit(false);
+         PreparedStatement st = conn.prepareStatement("INSERT INTO BillsOfQuantities VALUES (?);");
+         st.setInt(1, supplierId);
+         int rowNum = st.executeUpdate();
+         st.close();
+         if (rowNum != 0) {
+             conn.commit();
+         } else {
+             conn.rollback();
+             conn.close();
+             return false;
+         }
+         Set<Integer> ids = bill.keySet();
+         for (int i = 0; i < bill.size(); i++) {
+             st = conn.prepareStatement("INSERT INTO ItemsInBills VALUES (?,?,?,?);");
+             st.setInt(1, supplierId);
+             st.setInt(2, ids.iterator().next()); //no fucking idea if works
+             st.setInt(3, bill.get(i).getKey());
+             st.setDouble(4, bill.get(i).getValue());
+             rowNum = st.executeUpdate();
+             st.close();
+             if (rowNum != 0) {
+                 conn.commit();
+             } else {
+                 conn.rollback();
+                 conn.close();
+                 return false;
+             }
+         }
+         return true;
+     } catch (Exception e) {
+         System.err.println(e.getClass().getName() + ": " + e.getMessage());
+         return false;
+     }
+ }
+
+    public boolean addItemToAgreement(Integer suppId, Integer itemId, Double cost){
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:EOEDdatabase.db");
+            conn.setAutoCommit(false);
+            PreparedStatement st = conn.prepareStatement("INSERT INTO ItemsInAgreements VALUES (?,?,?);");
+            st.setInt(1, suppId);
+            st.setInt(2, itemId);
+            st.setDouble(3, cost);
+            int rowNum = st.executeUpdate();
+            st.close();
+            if (rowNum != 0) {
+                conn.commit();
+                conn.close();
+            } else {
+                conn.rollback();
+                conn.close();
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
         }
     }
-
-    public void addItemToAgreement(Integer supp_id ,Integer item_id,Double cost){
-        getSuppById(supp_id).addItemToAgreement(item_id,cost);
-    }
-
+/*
     public void updateBillOfQuantities(int supplierId, Integer itemId, Pair<Integer, Double> quantity_disc) {
        if(getSuppById(supplierId)!=null)
        {
@@ -113,22 +171,62 @@ public class SupplierMapper {
         return getSuppById(suppId).getItemIdByIndex(i);
     }
 
-    public void addItemToSupplier(int suppId, int itemId) {
+    public void addItemToSupplier(int suppId, int itemId) { //TODO already have addItemToAgreement - must have item price!
         this.getSuppById(suppId).addItemsToSupplier(itemId);
     }
 
     public int getItemsListSize(int suppId) {
         return this.getSuppById(suppId).getItemsListSize();
     }
+*/
+    public boolean setItemPrice(int suppId, int itemId, double newPrice) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:EOEDdatabase.db");
+            conn.setAutoCommit(false);
+            PreparedStatement st = conn.prepareStatement("UPDATE ItemsInAgreements SET price = ? WHERE agreementId = ?" +
+                    "AND suppId = (?);");
+            st.setDouble(1, newPrice);
+            st.setInt(2, suppId);
+            st.setInt(3, itemId);
+            int rowNum = st.executeUpdate();
+            st.close();
+            if (rowNum != 0) {
+                conn.commit();
+                conn.close();
+            } else {
+                conn.rollback();
+                conn.close();
+                return false;
+            }
+            return true;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
 
-    public void setItemPrice(int suppId, int itemId, double newPrice) {
-        getSuppById(suppId).setItemPrice(itemId, newPrice);
+        }
     }
 
     public boolean validateItemId(int suppId, int itemId) {
-        return getSuppById(suppId).validateItemId(itemId);
+        try {
+            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection("jdbc:sqlite:EOEDdatabase.db");
+            conn.setAutoCommit(false);
+            PreparedStatement st = conn.prepareStatement("SELECT * FROM ItemsInAgreements WHERE aggreementId = ? AND itemId = ?;");
+            st.setInt(1, suppId);
+            st.setInt(2, itemId);
+            int rowNum = st.executeUpdate();
+            st.close();
+            if (rowNum != 0) {
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            return false;
+        }
     }
-
+/*
     public int bestSuppForItem(Integer itemId, Integer quantity) {
         Double min=100000000.0;
         int bestSuppId=-1;
@@ -160,7 +258,7 @@ public class SupplierMapper {
         return getSuppById(bestSuppForItem).getDiscountOfItem(itemId, amount);
     }
 
-    public List<Object> getSuppDetails(int bestSuppForItem) {
+    public List<Object> getSuppDetails(int bestSuppForItem) { //TODO create dto
         return getSuppById(bestSuppForItem).getSuppDetails();
     }
 }
