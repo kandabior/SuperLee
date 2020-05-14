07b9b4c49848305acd84b4c5e5cdce2ceed19ff8
or kandabi <kandabior@gmail.com>
2020-05-14 13:42:17 +0300

    closed database

--- dev/DataAccessLayer/ReportMapper.java
+++ dev/DataAccessLayer/ReportMapper.java
@@ -3,21 +3,64 @@ package DataAccessLayer;
 import DTO.ReportDTO;
 import DTO.ReportLineDTO;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ReportMapper {
 
     private Connection c;
     public ReportMapper(){}
 
+    public int getReportId() {
+        PreparedStatement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:dev\\EOEDdatabase.db");
+            c.setAutoCommit(false);
+            stmt = c.prepareStatement("SELECT counter FROM Counters WHERE name=?;");
+            stmt.setString(1,"reports");
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                int output= rs.getInt("counter");
+                rs.close();
+                stmt.close();
+                stmt = c.prepareStatement("UPDATE Counters SET counter=? WHERE name=?;");
+                stmt.setInt(1,output+1);
+                stmt.setString(2,"reports");
+                stmt.executeUpdate();
+                c.commit();
+                c.close();
+                stmt.close();
+                return output;
+            } else {
+                rs.close();
+                stmt.close();
+                stmt = c.prepareStatement("INSERT Counters VALUES (reports,2);");
+                stmt.executeUpdate();
+                stmt.close();
+                c.commit();
+                c.close();
+                return 1;
+            }
+        } catch (Exception e) {
+            if (c != null) {
+                try {
+                    System.err.print("Transaction is being rolled back");
+                    c.rollback();
+                } catch (SQLException excep) {
+                }
+            }
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
+        return 0;
+
+    }
+
 
     public void addReport(ReportDTO reportDTO) {
         PreparedStatement stmt = null;
         try {
             Class.forName("org.sqlite.JDBC");
+
             c = DriverManager.getConnection("jdbc:sqlite:dev\\EOEDdatabase.db");
             c.setAutoCommit(false);
             stmt = c.prepareStatement("INSERT INTO Reports VALUES (?,?);");
@@ -47,9 +90,10 @@ public class ReportMapper {
             }
             c.close();
         } catch (Exception e) {
+            e.printStackTrace();
             if (c != null) {
                 try {
-                    System.err.print("Transaction is being rolled back");
+                    System.err.println("Transaction is being rolled back");
                     c.rollback();
                 } catch (SQLException excep) {
                 }
