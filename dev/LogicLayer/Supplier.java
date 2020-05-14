d956aa5482495a1ee120ee172679603c27dbac43
or kandabi <kandabior@gmail.com>
2020-05-14 15:33:40 +0300

    update

--- dev/LogicLayer/Supplier.java
+++ dev/LogicLayer/Supplier.java
@@ -103,7 +103,6 @@ public class Supplier {
 
     public Boolean checkBillOfQuantity(int agreementId) {
         //return this.supplierMapper.checkIfBillExists(agreementId);
-        return true;
     }
 
     public void addItemToBillOfQuantities(int itemId, int itemQuantity, Double itemDiscount) {
@@ -218,6 +217,6 @@ public class Supplier {
     }
 
     public void createBillOfQuantities(int suppId) {
-      //  this.supplierMapper.createBillOfQuantities(suppId);
+        this.supplierMapper.createBillOfQuantities(suppId);
     }
 }
