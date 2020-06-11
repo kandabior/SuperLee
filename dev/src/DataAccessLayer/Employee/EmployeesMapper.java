16d46da47d26f6a94443ad6d68530f41d638d5e6
or kandabi <kandabior@gmail.com>
2020-06-11 15:52:12 +0300

    fix elad

--- dev/src/DataAccessLayer/Employee/EmployeesMapper.java
+++ dev/src/DataAccessLayer/Employee/EmployeesMapper.java
@@ -188,7 +188,7 @@ public class EmployeesMapper {
 
     private boolean tryOpen() {
         try {
-            String url = "jdbc:sqlite:EOEDdatabase.db";
+            String url = "jdbc:sqlite:dev\\EOEDdatabase.db";
             con = DriverManager.getConnection(url);
             return true;
         } catch (SQLException e) {
--- dev/src/Tests/EmployeesManagerTest.java
+++ dev/src/Tests/EmployeesManagerTest.java
@@ -46,7 +46,7 @@
 //    @After
 //    public void deleteFromEmployeesTable(){
 //        try {
-//            String url = "jdbc:sqlite:EOEDdatabase.db";
+//            String url = "jdbc:sqlite:dev\\EOEDdatabase.db";
 //            con = DriverManager.getConnection(url);
 //            Class.forName("org.sqlite.JDBC");
 //            con.setAutoCommit(false);
